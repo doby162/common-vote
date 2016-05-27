31e297c1511eca530f9154f68ee6dcb472d38b88
Michael Acker <doby162@gmail.com>
2016-05-27 18:38:17 -0500

    normally wouldn't commit such a minor thing but I am about to take on canvas and I might need to revert

--- common-vote.lisp
+++ common-vote.lisp
@@ -76,7 +76,7 @@
 		(right-frame (make-instance `frame :master top-frame))
 		(vote-1 (make-instance `frame :master left-frame))
 		(vote-1-button (make-instance `button :master vote-1 :text "vote4me" :command (lambda () (format t "voted for one~%"))))
-		(vote-1-text (make-instance `button :master vote-1 :text "describe stuff")))
+		(vote-1-text (make-instance `label :master vote-1 :text "describe stuff")))
 	(pack top-frame)
 	(pack left-frame  :side :left )
 	(pack right-frame :side :right)
